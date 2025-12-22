30581d4b7c59f6cecf17f72aadfaa9711f8d889e
Declan <declan@declans.net>
2025-12-22 21:07:51 +0000

    Initial commit

---
+++ test/dynamic-branch-env.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DynamicBranchEnv from '../lib/dynamic-branch-env-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dynamic-branch-env-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DynamicBranchEnv.DynamicBranchEnvStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
